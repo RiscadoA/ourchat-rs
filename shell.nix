65afcb9485cfcea82280cd27a4e7ec29f58b29bf
Ricardo Antunes <me@riscadoa.com>
2021-11-21 10:18:17 +0000

    Updated shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 let
+  rust-version = "1.56.1";
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rust-channel = nixpkgs.rustChannelOf {
+    channel = rust-version;
+  };
+  rust = rust-channel.rust.override {
+    extensions = [ "rust-src" ];
+  };
+  cargo = rust-channel.cargo;
 in with nixpkgs; mkShell {
-  nativeBuildInputs = [
-    latest.rustChannels.stable.rust
-    pkgconfig
-    cargo
-    rls
+  buildInputs = [
+    rust cargo
   ];
 }
